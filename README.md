0875451c47f4c29da2a82b4139f09f65a620aa0f
Sang-Heon Jeon <ekffu200098@gmail.com>
2020-07-23 17:37:42 +0000

    Feat: add report structure
    Modify request structure so that old data can be read. And then, data.csv is no more used. Also constraint about start-date is removed. Result README.md have initial report structure.

--- README.md
+++ README.md
@@ -6,11 +6,10 @@ Every day at 9am, github action will check your contribution and update result.
 
 ## How to Use?
 1. Fork this project to your account :smile:
-2. Modify `.github/workflows/check.yml` and remove `result/data.csv`
+2. Modify `.github/workflows/check.yml`
 ``` yml
 env:
   # Change below variable to date you want to start tracking.
-  # This date(YYYY-MM-DD) must be within a year.
   start-date: '2020-07-01'
 ```
 3. Enjoy daily contribution checker :tada:
@@ -19,8 +18,6 @@ env:
 - We track your contribution history from your public profile with anonymous user.
   So, if you turn off `Contribution Settings > Private contributions`, private contribution will not be tracked.
 - Contribution of the public profile is based on UTC. It should be different from your login profile.
-- Data initialization is performed only the first time.
-  If you want to reset data, you need to delete `result/data.csv` manually.
 
 ## Bug report & Feature request
 - Please leave an [issue](https://github.com/lntuition/daily-contribution-checker/issues). Reporting makes the software more robust :+1:
--- src/main.py
+++ src/main.py
@@ -1,43 +1,13 @@
 import os
 import sys
 
-from data import (
-    append_csv_data,
-    generate_csv_data,
-    generate_markdown_table
-)
-from request import request_github_commit
-
-
-DATA_PATH = "result/data.csv"
-TABLE_PATH = "result/README.md"
+from data import DataManager
 
 if __name__ == "__main__":
-    config = {
-        "username": sys.argv[1],
-        "start-date": sys.argv[2],
-        "target-date": sys.argv[3],
-    }
-
-    commit_data = request_github_commit(
-        username=config["username"]
+    data = DataManager(
+        username=sys.argv[1],
+        start=sys.argv[2],
+        end=sys.argv[3],
     )
 
-    if not os.path.exists(DATA_PATH):
-        generate_csv_data(
-            start=config["start-date"],
-            target=config["target-date"],
-            path=DATA_PATH,
-            commits=commit_data
-        )
-
-    append_csv_data(
-        target=config["target-date"],
-        path=DATA_PATH,
-        commits=commit_data
-    )
-
-    generate_markdown_table(
-        data_path=DATA_PATH,
-        table_path=TABLE_PATH
-    )
+    data.generate_report(save_path="result/README.md")
