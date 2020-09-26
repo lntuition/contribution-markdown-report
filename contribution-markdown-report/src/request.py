a5b73f1f05bb4a0027f6c5833e431783d60e9535
Sang-Heon Jeon <ekffu200098@gmail.com>
2020-09-26 14:34:28 +0900

    Chore: change exception message

--- contribution-markdown-report/src/request.py
+++ contribution-markdown-report/src/request.py
@@ -10,6 +10,6 @@ def fetch_raw_text(url: str) -> str:
 
     status = response.status_code
     if status != 200:
-        raise RequestException(f"[{status}] is not 200")
+        raise RequestException(f"{status} : fail request")
 
     return response.text
--- contribution-markdown-report/src/util.py
+++ contribution-markdown-report/src/util.py
@@ -10,7 +10,7 @@ class EnvironException(Exception):
 def safe_environ(key: str, not_exist_ok: bool = False) -> Optional[str]:
     value = os.environ.get(key, None)
     if not not_exist_ok and not value:
-        raise EnvironException(f"[{key}] not exist")
+        raise EnvironException(f"{key} : not exist")
 
     return value
 
