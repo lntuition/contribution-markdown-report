58e312732408fadbaa2f89148e4780fa2af30484
Sang-Heon Jeon <ekffu200098@gmail.com>
2020-09-27 21:30:09 +0900

    Feat: add date source

---
+++ contribution-markdown-report/src/date.py
@@ -0,0 +1,70 @@
+from datetime import datetime, timedelta
+from typing import Iterable
+
+
+class DateFormatException(Exception):
+    pass
+
+
+class Date:
+    def __init__(self, date: str) -> None:
+        if date == "yesterday":  # Reserved keyword
+            dt = datetime.now() - timedelta(days=1)
+        else:
+            try:
+                dt = datetime.strptime(date, "%Y-%m-%d")
+            except ValueError as e:
+                raise DateFormatException(f"{date} : wrong format") from e
+
+        self.__year = dt.year
+        self.__month = dt.month
+        self.__day = dt.day
+
+    @property
+    def year(self) -> int:
+        return self.__year
+
+    @property
+    def month(self) -> int:
+        return self.__month
+
+    @property
+    def day(self) -> int:
+        return self.__day
+
+    def __lt__(self, other) -> bool:
+        return int(self) < int(other)
+
+    def __le__(self, other) -> bool:
+        return int(self) <= int(other)
+
+    def __ge__(self, other) -> bool:
+        return int(self) >= int(other)
+
+    def __gt__(self, other) -> bool:
+        return int(self) > int(other)
+
+    def __int__(self) -> int:
+        return (self.year * 10000) + (self.month * 100) + self.day
+
+    def __repr__(self) -> str:
+        return f"{self.__year}-{self.__month:02d}-{self.__day:02d}"
+
+
+class DateIntervalException(Exception):
+    pass
+
+
+class DateInterval:
+    def __init__(self, start: Date, end: Date) -> None:
+        if end > start:
+            raise DateIntervalException(f"{start} : eariler than {end}")
+
+        self.__start = start
+        self.__end = end
+
+    def iter_year(self) -> Iterable[str]:
+        return range(self.__start.year, self.__end.year + 1)
+
+    def is_between(self, date: str) -> bool:
+        return self.__start <= Date(date) <= self.__end
