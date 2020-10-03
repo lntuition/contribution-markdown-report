bfade3ec11d74f0248e33baf6de0af9634d57d8c
Sang-Heon Jeon <ekffu200098@gmail.com>
2020-10-03 11:38:45 +0900

    Refactor: apply migrated report generation

--- contribution-markdown-report/src/main.py
+++ contribution-markdown-report/src/main.py
@@ -1,76 +1,43 @@
-import os
-import sys
-import traceback
-from functools import reduce
-
 from crawler import Crawler
 from date import Date, DateInterval
-from language.factory import language_setting_factory
+from report import Report
 from repository import Repository, RepositoryURL
-from section.graph import GraphGenerator
-from section.header import HeaderGenerator
-from section.summary import SummaryGenerator
+from setting import SettingFactory
 from util import safe_chdir, safe_environ
+from writer import GraphWriter, HeaderWriter, SummaryWriter
 
 if __name__ == "__main__":
-    try:
-        user = safe_environ("GITHUB_ACTOR")
-        end = Date(date="yesterday")
+    user = safe_environ("GITHUB_ACTOR")
+    end = Date(date="yesterday")
 
-        repo_url = RepositoryURL(
-            user=user,
-            token=safe_environ("INPUT_GITHUB_TOKEN"),
-            name=safe_environ("GITHUB_REPOSITORY"),
-        )
-        repo = Repository(
-            url=repo_url,
-            path=safe_environ("OUTPUT_PATH"),
-            branch=safe_environ("INPUT_BRANCH"),
-        )
+    repo_url = RepositoryURL(
+        user=user,
+        token=safe_environ("INPUT_GITHUB_TOKEN"),
+        name=safe_environ("GITHUB_REPOSITORY"),
+    )
+    repo = Repository(
+        url=repo_url,
+        path=safe_environ("OUTPUT_PATH"),
+        branch=safe_environ("INPUT_BRANCH"),
+    )
 
-        data = Crawler.execute(
-            user=user,
-            interval=DateInterval(start=Date(date=safe_environ("INPUT_START_DATE")), end=end),
-        )
-        setting = language_setting_factory(language=safe_environ("INPUT_LANGUAGE"))
+    data = Crawler.execute(
+        user=user,
+        interval=DateInterval(start=Date(date=safe_environ("INPUT_START_DATE")), end=end),
+    )
+    settings = SettingFactory.create_settings(language=safe_environ("INPUT_LANGUAGE"))
 
-        generate_path = os.path.join(repo.workdir, safe_environ("INPUT_PATH"))
+    work_path = safe_environ("INPUT_PATH")
 
-        # Generate
-        with safe_chdir(generate_path), open("README.md", "w") as fp:
-            fp.write(
-                reduce(
-                    lambda a, b: a + b,
-                    map(
-                        lambda x: x.generate(),
-                        [
-                            HeaderGenerator(
-                                username=user,
-                                setting=setting,
-                            ),
-                            SummaryGenerator(
-                                data=data,
-                                setting=setting,
-                            ),
-                            GraphGenerator(
-                                data=data,
-                                setting=setting,
-                            ),
-                        ],
-                    ),
-                )
-            )
+    writers = [HeaderWriter(user=user), SummaryWriter(data=data), GraphWriter(data=data)]
 
-        repo.add(path=generate_path)
-        repo.commit(
-            msg=f"BOT : {end} contribution report",
-            email=safe_environ("INPUT_AUTHOR_EMAIL"),
-            name=safe_environ("INPUT_AUTHOR_NAME"),
-        )
-        repo.push()
+    with safe_chdir(repo.workdir):
+        Report.generate(writers=writers, settings=settings, path=work_path)
 
-        sys.exit(0)
-    except Exception:
-        traceback.print_exc()
-
-        sys.exit(1)
+    repo.add(path=work_path)
+    repo.commit(
+        msg=f"BOT : {end} contribution report",
+        email=safe_environ("INPUT_AUTHOR_EMAIL"),
+        name=safe_environ("INPUT_AUTHOR_NAME"),
+    )
+    repo.push()
