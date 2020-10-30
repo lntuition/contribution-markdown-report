c47a606fae4ff672c904ee0831cea38745b3246a
Sang-Heon Jeon <ekffu200098@gmail.com>
2020-10-30 15:49:27 +0000

    Feat: add barplot related classes

---
+++ contribution-markdown-report/src/graph.py
@@ -0,0 +1,86 @@
+from typing import List
+
+import seaborn
+from matplotlib.axes import Axes
+from matplotlib.figure import Figure
+from matplotlib.text import Annotation
+from pandas import Series
+
+
+class BarplotAxesBuilder:
+    @staticmethod
+    def build(
+        series: Series,
+        *,
+        style: str = "whitegrid",
+        palette: str = "pastel",
+    ) -> Axes:
+        figure = Figure(tight_layout=True)
+        axes = figure.add_subplot()
+
+        seaborn.set_theme(style=style, palette=palette)
+        seaborn.barplot(x=series.index, y=series, ax=axes)
+        seaborn.despine(ax=axes)
+
+        return axes
+
+
+class Barplot:
+    def __init__(self, axes: Axes) -> None:
+        self.__axes = axes
+
+    def get_xticklabels(self) -> List[str]:
+        return [xticklabel.get_text() for xticklabel in self.__axes.get_xticklabels()]
+
+    def get_xlabel(self) -> str:
+        return self.__axes.get_xlabel()
+
+    def get_ylabel(self) -> str:
+        return self.__axes.get_ylabel()
+
+    def get_annotations(self) -> List[str]:
+        return [child.get_text() for child in self.__axes.get_children() if isinstance(child, Annotation)]
+
+    def set_xticklabels(self, labels: List[str]) -> None:
+        xticklabels = self.__axes.get_xticklabels()
+
+        for xticklabel in xticklabels:
+            old_text = xticklabel.get_text()
+
+            idx = int(old_text)
+            new_text = labels[idx]
+
+            xticklabel.set_text(new_text)
+
+        self.__axes.set_xticklabels(xticklabels)
+
+    def set_xlabel(self, label: str) -> None:
+        self.__axes.set_xlabel(label)
+
+    def set_ylabel(self, label: str) -> None:
+        self.__axes.set_ylabel(label)
+
+    def set_annotations(
+        self,
+        *,
+        ha: str = "center",
+        va: str = "bottom",
+    ) -> None:
+        for patch in self.__axes.patches:
+            x = patch.get_x()
+            y = patch.get_y()
+
+            height = patch.get_height()
+            width = patch.get_width()
+
+            if round(height, 2) != 0:
+                self.__axes.annotate(
+                    text=f"{height:.0f}" if height.is_integer() else f"{height:.2f}",
+                    xy=(x + width / 2, y + height * 1.025),
+                    ha=ha,
+                    va=va,
+                )
+
+    def save(self, file_name: str) -> None:
+        figure = self.__axes.get_figure()
+        figure.savefig(file_name)
---
+++ contribution-markdown-report/test/unit/test_graph.py
@@ -0,0 +1,98 @@
+import os
+from typing import List
+from unittest.mock import MagicMock, patch
+
+import pytest
+import seaborn
+from matplotlib.axes import Axes
+from matplotlib.figure import Figure
+from pandas import Series
+
+from src.graph import Barplot, BarplotAxesBuilder
+
+
+class TestBarplotAxesBuilder:
+    def build_barplot_axes(
+        self,
+        *,
+        index: List[int],
+        data: List[int],
+    ) -> Axes:
+        return BarplotAxesBuilder.build(
+            series=Series(
+                index=index,
+                data=data,
+            )
+        )
+
+    @patch("seaborn.set_theme")
+    def test_build(self, mock_set_theme: MagicMock) -> None:
+        index = [7, 8, 9, 10, 11]
+        data = [23, 0, 13, 42, 38]
+
+        barplot_axes = self.build_barplot_axes(index=index, data=data)
+
+        assert ["7", "8", "9", "10", "11"] == [xticklabel.get_text() for xticklabel in barplot_axes.get_xticklabels()]
+        assert data == [patch.get_height() for patch in barplot_axes.patches]
+
+        assert not barplot_axes.spines["right"].get_visible()
+        assert not barplot_axes.spines["top"].get_visible()
+
+
+class TestBarplot:
+    def build_barplot(
+        self,
+        *,
+        index: List[int] = [0, 1],
+        data: List[int] = [2, 3],
+    ) -> Barplot:
+        figure = Figure(tight_layout=True)
+        axes = figure.add_subplot()
+
+        seaborn.barplot(x=index, y=data, ax=axes)
+
+        return Barplot(axes=axes)
+
+    def test_set_xticklabels(self) -> None:
+        index = [5, 4, 3, 2, 1]
+        data = [23, 0, 13, 42, 38]
+        labels = ["zero", "one", "two", "three", "four", "five", "six"]
+
+        barplot = self.build_barplot(index=index, data=data)
+        barplot.set_xticklabels(labels=labels)
+
+        assert ["one", "two", "three", "four", "five"] == barplot.get_xticklabels()
+
+    def test_set_xlabel(self) -> None:
+        xlabel = "xlabel"
+
+        barplot = self.build_barplot()
+        barplot.set_xlabel(label=xlabel)
+
+        assert xlabel == barplot.get_xlabel()
+
+    def test_set_ylabel(self) -> None:
+        ylabel = "ylabel"
+
+        barplot = self.build_barplot()
+        barplot.set_ylabel(label=ylabel)
+
+        assert ylabel == barplot.get_ylabel()
+
+    def test_set_annotations(self) -> None:
+        index = [1, 2, 3, 4, 5]
+        data = [23.256, 23.254, 0, 1, 19]
+
+        barplot = self.build_barplot(index=index, data=data)
+        barplot.set_annotations()
+
+        assert ["23.26", "23.25", "1", "19"] == barplot.get_annotations()
+
+    @pytest.mark.usefixtures("fake_root")
+    def test_save(self) -> None:
+        file_name = "file_name.png"
+
+        barplot = self.build_barplot()
+        barplot.save(file_name=file_name)
+
+        assert os.path.isfile(file_name)
