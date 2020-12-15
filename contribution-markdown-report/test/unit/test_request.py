e6cb992516cadcc63f6a270c50e1729e5e980563
Sang-Heon Jeon <ekffu200098@gmail.com>
2020-12-15 19:20:07 +0000

    Chore: remove unnecessary test group

--- contribution-markdown-report/test/unit/test_request.py
+++ contribution-markdown-report/test/unit/test_request.py
@@ -4,29 +4,29 @@ import responses
 from src.request import Request
 
 
-class TestRequest:
-    @responses.activate
-    def test_fetch_text_success(self) -> None:
-        url = "http://github.com/mocking/success"
-        key = "key"
-        value = "value"
-        responses.add(
-            responses.GET,
-            f"{url}?{key}={value}",
-            body=b"text",
-        )
+@responses.activate
+def test_fetch_text_success() -> None:
+    url = "http://github.com/mocking/success"
+    key = "key"
+    value = "value"
+    responses.add(
+        responses.GET,
+        f"{url}?{key}={value}",
+        body=b"text",
+    )
 
-        text = Request.fetch_text(url, {key: value})
-        assert text == "text"
+    text = Request.fetch_text(url, {key: value})
+    assert text == "text"
 
-    @responses.activate
-    def test_fetch_text_fail(self) -> None:
-        url = "http://github.com/mocking/fail"
-        responses.add(
-            responses.GET,
-            url,
-            status=503,
-        )
 
-        with pytest.raises(Exception):
-            Request.fetch_text(url)
+@responses.activate
+def test_fetch_text_fail() -> None:
+    url = "http://github.com/mocking/fail"
+    responses.add(
+        responses.GET,
+        url,
+        status=503,
+    )
+
+    with pytest.raises(Exception):
+        Request.fetch_text(url)
--- contribution-markdown-report/test/unit/test_skeleton.py
+++ contribution-markdown-report/test/unit/test_skeleton.py
@@ -3,20 +3,18 @@ import pytest
 from src.skeleton import MessageSkeleton
 
 
-class TestMessageSkeleton:
-    @pytest.mark.parametrize(
-        "language",
-        [
-            "english",
-        ],
-    )
-    def test_get_skeleton(self, language: str) -> None:
-        skeleton = MessageSkeleton.get_skeleton(language)
+@pytest.mark.parametrize(
+    "language",
+    ["english"],
+)
+def test_get_skeleton(language: str) -> None:
+    skeleton = MessageSkeleton.get_skeleton(language)
 
-        assert skeleton == MessageSkeleton.skeleton[language]
+    assert skeleton == MessageSkeleton.skeleton[language]
 
-    def test_get_skeleton_with_warning(self) -> None:
-        with pytest.warns(UserWarning):
-            skeleton = MessageSkeleton.get_skeleton("NOTSUPPORTED")
 
-        assert skeleton == MessageSkeleton.skeleton["english"]
+def test_get_skeleton_with_warning() -> None:
+    with pytest.warns(UserWarning):
+        skeleton = MessageSkeleton.get_skeleton("NOTSUPPORTED")
+
+    assert skeleton == MessageSkeleton.skeleton["english"]
